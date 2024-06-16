762af7f77fc05c4600e60c4070e45fd73082b040
jerboscolo <jeremy@boscolo.net>
2024-06-16 09:09:52 -0500

    onchain is spelled onchain

--- components/Description.tsx
+++ components/Description.tsx
@@ -11,10 +11,10 @@ export default function Description() {
         <div style={{color: "var(--brand-text-main)"}} className="w-full flex justify-center pt-8">
           <div className="max-w-[720px] px-6 w-full flex flex-col gap-4">
             <h2>
-            The Beautiful Game: On-Chian
+            The Beautiful Game Onchain
             </h2>
             <p>
-              Coppa Nouns is a football-loving DAO that brings the beautiful game on-chain. 
+              Coppa Nouns is a football-loving DAO that brings the beautiful game onchain. 
               When you mint a Coppa Noun, the kit your Coppa Noun wears determines which country it represents. 
               During major tournaments, owners of Coppa Nouns have the opportunity to win rewards for the country they represent.
               </p>
@@ -23,7 +23,7 @@ export default function Description() {
               These rewards are then distributed back to the community of Coppa Noun holders based on how well their country plays.
               </p>
               <p>
-              If you believe in a team or want to support your favorite country, mint a Coppa Noun and help us bring the beautiful game on-chain.
+              If you believe in a team or want to support your favorite country, mint a Coppa Noun and help us bring the beautiful game onchain.
               </p>
           </div>
         </div>
